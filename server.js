493c1078996db9c464fb93c7b3a3116cdc46e92c
Porky Envy <codexwebs@gmail.com>
2019-01-05 03:18:40 +0200

    Update server.js
    modified

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var login = require("./routes/usersManagers/login");
 var commerces = require("./routes/commerces");
 var app = express();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0	'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var socket_io = require("socket.io");
 
