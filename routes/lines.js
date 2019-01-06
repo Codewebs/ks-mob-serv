255080264596f3c33803726c1fb6f14ff37deb46
Porky Envy <codexwebs@gmail.com>
2019-01-06 15:57:30 +0200

    Add files via upload

--- routes/lines.js
+++ routes/lines.js
@@ -7,9 +7,8 @@ var db = mongojs("mongodb://Droidbot:Indiza__2018@ds257752.mlab.com:57752/kusani
 
 //Get AllLines by city and emit track by user
 router.get("/linesDisplay/:cityID", function(req, res, next){
-	var io = req.app.io;
-    console.log(req);
-    db.commercesLocation.find({city: req.params.city},function(err, liners){
+	var io = req.app.io;   
+    db.lines.find({city: req.params.cityID},function(err, liners){
         if (err){
             res.send(err);
         }
@@ -17,25 +16,54 @@ router.get("/linesDisplay/:cityID", function(req, res, next){
         io.emit("trackLine", liners);
     });
 });
-
+router.get("/lines", function(req,res, next){
+	db.lines.find(function(err,lines){
+		if(err){
+			res.send(err);
+		}
+		res.json(lines);
+	})
+});
 //Get a Ligne whith his datas
-router.get("/line/:id", function(rea, res, next){
+router.get("/line/:id", function(req, res, next){
 	var io = req.app.io;
-	db.lines.findOne({lineId:req.params.id}, function(err, liner){
+	console.log(req.params)
+	db.lines.findOne({lineId: req.params.id}, function(err, liner){
 		if(err){
 			res.send(err);
 		}
 		res.send(liner);
-
 	})
-})
+});
 
 
 // Ajouter une nouvelle ligne et definir sa route
-router.post("/addline/", function(req, res, next){
-	var line = req.body.data;
+router.get("/lines/", function(req, res, next){
+console.log(req.params)
+	var line = {
+    "lineId": "55E",
+    "lineName": "ligne 5",
+    "distance": "40500",
+    "city": "5c2f1453fb6fc02c41a58ce2",
+    "startpoint": {
+        "type": "Point",
+        "coordinates": [
+            11.5394185,
+            3.8448469
+        ]
+    },
+    "endpoint": {
+        "type": "Point",
+        "coordinates": [
+            11.5394185,
+            3.8448469
+        ]
+    },
+    "estimated": "35",
+    "fare": "250"
+}
 	var io = req.app.io;
-
+	
 	if(!line.libelle){
 		res.status(400);
 		res.json({
